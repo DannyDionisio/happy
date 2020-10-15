e73a8194c875e463191069749bcde161e376de23
Daniela Dionísio <dannyy.dionisio@gmail.com>
2020-10-15 15:07:02 +0100

    first commit

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FiArrowRight } from 'react-icons/fi'
+
+import '../assets/styles/pages/landing.css'
+
+import logoImg from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
+
+function Landing() {
+	return (
+		<div id='page-landing'>
+			<div className='content-wrapper'>
+				<img src={logoImg} />
+
+				<main>
+					<h1>Leve felicidade para o mundo</h1>
+					<p>Visite orfanatos e mude o dia de muitas crianças.</p>
+				</main>
+
+				<div className='location'>
+					<strong>Amadora</strong>
+					<span>Lisboa</span>
+				</div>
+				<Link to='/app' className='enter-app'>
+					<FiArrowRight size={26} color='rgba(0, 0, 0, 0.6)' />
+				</Link>
+			</div>
+		</div>
+	)
+}
+
+export default Landing
